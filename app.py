be939ecc635959aaef2ece8217406b576a8a34d2
calebjubal <2301730055@krmu.edu.in>
2026-02-18 18:37:52 +0530

    Add Flask application setup in app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+# Create a Flask application instance
+# __name__ helps Flask determine the root path of the application
+app = Flask(__name__)
+
+# The route() decorator tells Flask what URL should trigger the function
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# This block ensures the development server runs only when the script is executed directly
+if __name__ == "__main__":
+    # Run the app. By default, it runs on http://127.0.0.1:5000/
+    app.run(debug=True)
