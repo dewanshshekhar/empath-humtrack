35b3745031cc1db474bd1547e688d6eed5814b16
Dewansh shekhar Singh <143202935+dewanshshekhar@users.noreply.github.com>
2026-02-19 21:06:14 +0530

    Update __init__.py

--- empath/ui/language_segmentation/__init__.py
+++ empath/ui/language_segmentation/__init__.py
@@ -1,4 +1,4 @@
-from acestep.language_segmentation.LangSegment import LangSegment
+from empath.language_segmentation.LangSegment import LangSegment
 
 
 # release
@@ -7,3 +7,4 @@ __version__ = "0.3.5"
 
 # develop
 __develop__ = "dev-0.0.1"
+
